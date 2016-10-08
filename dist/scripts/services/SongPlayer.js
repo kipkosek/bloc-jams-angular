d0c7a23551daa9c32e5cf4a6912636f57d511399
Kip Kosek <kipkosek@gmail.com>
2016-10-07 20:59:48 -0400

    Assignment 8 Services 3: Completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -38,7 +38,15 @@
             currentBuzzObject.play();
             song.playing = true;
         };
-
+        /**
+        * @function stopSong
+        * @desc Stops playing audio file assigned to currentBuzzObject
+        * @param {Object} song
+        */
+        var stopSong = function(song) {
+            currentBuzzObject.stop();
+            song.playing = null;
+        };
         /**
         * @function getSongIndex
         * @desc Gets index of current song
@@ -89,8 +97,24 @@
             currentSongIndex--;
 
             if (currentSongIndex < 0){
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+              stopSong();
+            } else {
+              var song = currentAlbum.songs[currentSongIndex];
+              setSong(song);
+              playSong(song);
+            }
+        };
+        /**
+        * @function SongPlayer.next
+        * @desc Plays song that follows the current one
+        * @param {Object} song
+        */
+        SongPlayer.next = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+
+            if (currentSongIndex >= currentAlbum.songs.length) {
+              stopSong();
             } else {
               var song = currentAlbum.songs[currentSongIndex];
               setSong(song);
